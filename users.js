53e8c9b84a38d332e95a7e4960c18e94d9f323ae
amanshuklam <amanshuklam711@gmail.com>
2026-09-17 11:35:55 +0530

    lab4

---
+++ users.js
@@ -0,0 +1,11 @@
+// we use in memmory data base for this example
+let users = [
+    {id:1,name:"Aman Shukla ",mob:"1234567890",email:"aman@gmail.com"},
+    {id:2,name:"user2",mob:"0987654321",email:"user2@gmail.com"}
+]
+let nextId = 3;
+export const getAllUsers =()=>users;
+
+
+
+
